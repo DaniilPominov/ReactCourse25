1f2a9176eabe9e4dca6c72654f5699b5a776c29f
DaniilPominov <d.pominov2016@yandex.ru>
2025-03-21 23:27:22 +0700

    Отдельные страницы товаров

--- src/components/OnlineStore/ProductCard.jsx
+++ src/components/OnlineStore/ProductCard.jsx
@@ -1,14 +1,17 @@
 import { React} from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "../../styles/card.css"
 function ProductCard(props){
     const product = props.product;
     const addToCart = props.action;
     const actionDesc = props.actionDesc;
+    const path = "/products/"+product.id;
     return (
         <li key={product.id} class="category-item">
+            <Link to={path}>
             <img src={product.product_img} alt={product.name} />
                         <h2>{product.name}</h2>
-                        <p>{product.description}</p>
+                        </Link>
                         <p>Price: ${product.price}</p>
                         
                         <button onClick={() => addToCart(product)}>{actionDesc}</button>
--- src/components/OnlineStore/pages/Product.jsx
+++ src/components/OnlineStore/pages/Product.jsx
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import { useParams } from "react-router-dom";
-
-const products = {
-    1: {name: "Dog Food", description: "The most delicious", price: "$20"},
-    2: {name: "Toy Ball", description: "The best for cats", price: "$5"},
-    3: {name: "Fish Tank", description: "Big and comfortable, ur fish will be good", price: "$50"}
-}
+import SupabaseContext from "../SupabaseContext";
 
 function Product() {
     const {id} = useParams();
-    const product = products[id];
-
-    if (!product) {
-        return <h1>Product not found</h1>
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const supabase = useContext(SupabaseContext);
+    useEffect(() => {
+            async function fetchProducts() {
+                
+                try {
+                    const { data, error } = await supabase
+                        .from('products')
+                        .select('*')
+                        .eq('id', id);
+                    
+                        if (error) {
+                        console.error(error);
+                    } else {
+                        setProduct(data[0]);
+                    }
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            } finally {
+                setLoading(false);
+            }
+            
+            }
+            fetchProducts().catch((error) => {
+                console.error("Error fetching products:", error);
+            });
+        }, [id, supabase]);
+    if (loading) {
+        return <h1>Loading...</h1>
     }
 
     return (
         <div>
-            <h1>{product.name}</h1>
-            <p>{product.description}</p>
+            <h2>{product.name}</h2>
+            <img src={product.product_img} alt={product.name} />
+            <p>О товаре: {product.description}</p>
             <p>Price: {product.price}</p>
         </div>
     )
